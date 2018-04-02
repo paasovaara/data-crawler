164f3d298f098d22649f35b234e4edfebf599f4d
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-04-02 14:56:28 +0300

    Added logging boilerplate

--- importer/main.py
+++ importer/main.py
@@ -1,3 +1,12 @@
+import logging
 
-print('hello, start the import')
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
+if __name__ == '__main__':
+    logger.info('Launching app')
