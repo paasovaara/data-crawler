7d1d3351b8542d7a9787e524ffa3c9f0f88cd5b6
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-04-08 08:56:52 +0300

    Create table if doesn't exist

--- importer/datamodel.py
+++ importer/datamodel.py
@@ -4,19 +4,42 @@ import sqlite3
 logger = logging.getLogger()
 DB_NAME="data.db"
 
+
+# TODO create separate SQL script
+CREATE_TABLE="""CREATE TABLE TextData(
+    id UNSIGNED BIG INT PRIMARY KEY NOT NULL,
+    created INT NOT NULL,
+    name VARCHAR(255),
+    filepath VARCHAR(255),
+    data TEXT
+);"""
+
 class DataModel(object):
+    conn = None
+    c = None
+
+    def __init__(self):
+        self.conn = sqlite3.connect(DB_NAME)
+        self.c = self.conn.cursor()
+
+    def doesDbExist(self):
+        self.c.execute('SELECT name FROM sqlite_master WHERE type=\'table\';')
+        result = self.c.fetchall()
+        return (len(result) > 0)
 
     def initialize(self):
-        conn = sqlite3.connect(DB_NAME)
-        c = conn.cursor()
+        self.c.execute('SELECT name FROM sqlite_master WHERE type=\'table\';')
+        result = self.c.fetchall()
+        if (not self.doesDbExist()):
+            if (not self.createDb()):
+                logger.error('FAILED TO CREATE DATABASE')
 
-        c.execute('SELECT name FROM sqlite_master WHERE type=\'table\';')
-        result = c.fetchall()
-        if (len(result) == 0):
-            self.createDb()
         else:
             logger.info('Database initialized OK')
 
 
     def createDb(self):
         logger.warn('Creating the database tables')
+        self.c.execute(CREATE_TABLE)
+        self.conn.commit()
+        return self.doesDbExist()
