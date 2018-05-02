a0f9ca7d3ecd379aa745695fa7ee1a531c4825a0
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-05-02 06:11:21 +0300

    import env in start scripts

--- run_import.sh
+++ run_import.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source .env/bin/activate
 python ./importer/main.py import --root .
 
--- run_process.sh
+++ run_process.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source .env/bin/activate
+
 NAME=${1:-foo}
 python ./importer/main.py process --name $NAME
 
