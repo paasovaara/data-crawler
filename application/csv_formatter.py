30aef421c3194008f1b0556acb383c5a80a95921
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-05-22 11:10:46 +0300

    add zeros to data if data missing

--- application/csv_formatter.py
+++ application/csv_formatter.py
@@ -58,40 +58,20 @@ def _formatSimpleRowForCSV(name, keywords, counter):
     return name + ',' + prefix + "," + year + "," + row
 
 
- # def _formatAllDataToSingleRowForCSV(prefix, allDataForPrefix, keywords, allCounters):
- #     sortedKeys = sorted(keywords)
- #     sortedValues = []
- #     for key in sortedKeys:
- #         for year in allDataForPrefix
- #         sortedValues.append(str(counter[key]))
- #
- #     metadata = _parseDataFromName(name)
- #     prefix = metadata[1]
- #     year = metadata[2]
- #     row = ','.join(sortedValues)
- #     return name + ',' + prefix + "," + year + "," + row
-
-
 _YEARS_PER_KEYWORD = 10
 
 
 def colFormatForCSV(keywords, allCounters):
 
     sortedKeys = sorted(keywords)
-    # This is a very specific case. TODO generalize somehow
+    # This is a very specific case. TODO generalize somehow, if possible. probably not
     colYears = sorted(range(2017 - _YEARS_PER_KEYWORD + 1, 2018), reverse=True)
     colHeaders = []
     for key in sortedKeys:
         for colYear in colYears:
             colHeaders.append(key + ' ' + str(colYear))
 
-    #sortedNames = sorted(allCounters.keys())
-    #for name in sortedNames:
-    #    valueRow = _formatRowForCSV(name, sortedKeys, allCounters[name])
-    #    valueRows.append(valueRow)
-
     headerRow = 'prefix,' + ','.join(colHeaders) + '\n'
-    #valueRows = []
 
     # first pre-process/augment the data
     keysWithPrefixAndYear = map(_parseDataFromName, allCounters.keys())
@@ -107,22 +87,19 @@ def colFormatForCSV(keywords, allCounters):
         for keyWord in sortedKeys:
 
             # sort by year, latest first
-            index = 0
-            # This has a bug, in case some entry has less than 10 years of data.
-
-            for data in sortedData:
-                index += 1
-                if index <= _YEARS_PER_KEYWORD:
+            for n in range(0, _YEARS_PER_KEYWORD):
+                try:
+                    data = sortedData[n]
                     _logger.info('%s, source data %s: prefix %s, year %s ', keyWord, data[0], data[1], data[2])
                     filename = data[0]
                     counter = allCounters[filename]
                     count = str(counter[keyWord])
                     dataRow.append(count)
+                except IndexError:
+                    dataRow.append('0')
 
         dataRows.append(','.join(dataRow))
 
-
     return headerRow + '\n'.join(dataRows) + '\n'
-    #return headerRow + '\n'.join(valueRows) + '\n'
 
 
